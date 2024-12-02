f484f3da69da37e71c455df30801d1161aeae764
isaiasvas <isaias@visioit.com.br>
2024-12-01 23:40:52 -0300

    Fix: autenticação obrigatoria para tarefas

--- app/routes/api.php
+++ app/routes/api.php
@@ -7,21 +7,21 @@ use Illuminate\Support\Facades\Route;
 
 // Rota de login
 Route::post('/login', [LoginController::class, 'login'])->name('login');
+Route::post('/users', [UserController::class, 'store']);
 
-// Rotas de tarefas
-Route::get('tasks', [TaskController::class, 'index']);
-Route::post('tasks', [TaskController::class, 'store']);
-Route::get('tasks/{task}', [TaskController::class, 'show']);
-Route::put('tasks/{task}/status', [TaskController::class, 'updateStatus']);
-Route::delete('tasks/{task}', [TaskController::class, 'destroy']);
 
 // Rotas autenticadas
 Route::middleware(['auth:sanctum'])->group(function () {
-   
+    
+    // Rotas de tarefas
+    Route::get('tasks', [TaskController::class, 'index']);
+    Route::post('tasks', [TaskController::class, 'store']);
+    Route::get('tasks/{task}', [TaskController::class, 'show']);
+    Route::put('tasks/{task}/status', [TaskController::class, 'updateStatus']);
+    Route::delete('tasks/{task}', [TaskController::class, 'destroy']);
 
     // Rotas de usuários
     Route::get('/users', [UserController::class, 'index']);
-    Route::post('/users', [UserController::class, 'store']);
     Route::get('/users/{user}', [UserController::class, 'show']);
     Route::put('/users/{user}', [UserController::class, 'update']);
     Route::delete('/users/{user}', [UserController::class, 'destroy']);
